c9d88f4037dd171395acbfebc3c70351c6ba9c04
anastasiia.mahalias <anastasiia.mahalias@powercode.us>
2023-10-31 11:52:39 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { Counter } from './components/Counter';
+import './styles/index.scss';
+import { Link, Route, Routes } from 'react-router-dom';
+import { Suspense } from 'react'
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
+import { MainPageAsync } from './pages/MainPage/MainPage.async';
+import { useTheme } from './theme/useTheme';
+import { classNames } from './helpers/classNames/classNames'
+
+const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <div className={classNames('app', {}, [theme])}>
+      <button onClick={toggleTheme}>TOGGLE</button>
+      <Link to={'/'}>Main</Link>
+      <Link to={'/about'}>About us</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path={'/about'} element={<AboutPageAsync />} />
+          <Route path={'/'} element={<MainPageAsync />} />
+        </Routes>
+      </Suspense>
+      <Counter />
+    </div>
+  )
+}
+
+export default App;
